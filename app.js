a3d6bad3b885c1f81294f789d74ec73797d4a02f
Roman Lesnoy <romanlesnoy@outlook.com>
2021-04-19 17:41:58 +0300

    Вынести центральный обработчик ошибок в отдельный файл

--- app.js
+++ app.js
@@ -6,8 +6,9 @@ const bodyParser = require('body-parser');
 const helmet = require('helmet');
 const { errors } = require('celebrate');
 const { requestLogger, errorLogger } = require('./middlewares/logger');
-const limiter = require('./utils/limiter');
+const limiter = require('./middlewares/limiter');
 const router = require('./routes/index');
+const { handleErrors } = require('./middlewares/handleErrors');
 const { DEV_DATA_BASE_PATH } = require('./utils/config');
 
 const app = express();
@@ -20,27 +21,21 @@ mongoose.connect(NODE_ENV === 'production' ? PRODUCTION_DATA_BASE_PATH : DEV_DAT
 });
 
 app.use(cors());
+
 app.use(bodyParser.json());
 
 app.use(helmet());
+
 app.use(requestLogger);
+
 app.use(limiter);
+
 app.use('/', router);
 
 app.use(errorLogger);
 
 app.use(errors());
 
-app.use((err, req, res, next) => {
-  const { statusCode = 500, message } = err;
-  res
-    .status(statusCode)
-    .send({
-      message: statusCode === 500
-        ? 'На сервере произошла ошибка'
-        : message,
-    });
-  next();
-});
+app.use(handleErrors);
 
 app.listen(PORT);
